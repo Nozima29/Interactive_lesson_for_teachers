0b1594d0aeedbb8e885dbe1f875a0c220f5d5e71
Nozima Murodova <32864688+Nozima29@users.noreply.github.com>
2019-12-13 00:27:52 +0500

    Update Teachers.java

--- src/teachers/Teachers.java
+++ src/teachers/Teachers.java
@@ -3,21 +3,13 @@ package teachers;
 //import java.sql.SQLException;
 
 public class Teachers {
-//    public static void main(String[] args) throws SQLException {
-//        //new Login().setVisible(true);
-//        //new Frame().setVisible(true);
-//        
-//    }
+    public static void main(String[] args) throws SQLException {
+        new Login().setVisible(true);
+        new Frame().setVisible(true);
+        
+    }
   
     
-    public static void main(String[] args){
-        int r1=1, r2=2, r3, r4=3;
-        r3=r1+r2;
-        r2=r3-r1;
-        r3=r4*r2;
-        r2=r2+r3;
-        
-        System.out.println(r2);
-    }
+    
 }
 
